08d1d852981a6b28cf82ed846efc662659ec91f7
Chirag-Jani <chirag@empiricinfotech.com>
2024-10-14 09:47:13 +0530

    Add links to open PDFs and footer text in Footer component.

--- client/src/layout/footer/Footer.tsx
+++ client/src/layout/footer/Footer.tsx
@@ -11,37 +11,59 @@ const Footer: React.FC = () => {
           className=" w-full h-auto max-h-28 object-cover"
         />
       </div>
+      <div className="flex flex-col gap-5 mx-6">
+        {/* Footer content  */}
+        <p className="text-gray-300 text-sm">
+          This does not serve as investment advice, nor does it represent an
+          invitation or endorsement to participate in any cryptocurrency
+          transactions. Investing in crypto assets involves the risk of losing
+          all or part of your investment, along with the possibility of
+          considerable price fluctuations, which may not be suitable for
+          individual investors. Historical financial results do not assure
+          future outcomes. Only invest what you can afford to lose. For further
+          information on the associated risks, please refer to the provided
+          link.
+        </p>
 
-      {/* Links Container */}
-      <div className="flex flex-col md:flex-row items-center text-white mt-4 md:mt-0">
-        <div className="flex flex-wrap justify-center md:justify-start">
-          <a
-            href="/terms"
-            className="hover:underline hover:text-blue-500 mx-2 mb-2"
-          >
-            Terms of Use
-          </a>
-          <span className="text-gray-400">|</span>
-          <a
-            href="/copyright"
-            className="hover:underline hover:text-blue-500 mx-2 mb-2"
-          >
-            Copyright
-          </a>
-          <span className="text-gray-400">|</span>
-          <a
-            href="/feedback"
-            className="hover:underline hover:text-blue-500 mx-2 mb-2"
-          >
-            Feedback
-          </a>
-          <span className="text-gray-400">|</span>
-          <a
-            href="/support"
-            className="hover:underline hover:text-blue-500 mx-2 mb-2"
-          >
-            Support
-          </a>
+        {/* Links Container */}
+        <div className="flex flex-col md:flex-row items-center justify-end text-white mt-4 md:mt-0">
+          <div className="flex flex-wrap justify-center md:justify-start">
+            <a
+              href="/privacy-policy.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline hover:text-blue-500 mx-2 mb-2"
+            >
+              Terms of Use
+            </a>
+            <span className="text-gray-400">|</span>
+            <a
+             href="/privacy-policy.pdf"
+             target="_blank"
+             rel="noopener noreferrer"
+              className="hover:underline hover:text-blue-500 mx-2 mb-2"
+            >
+              Copyright
+            </a>
+            <span className="text-gray-400">|</span>
+            <a
+              href="/privacy-policy.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline hover:text-blue-500 mx-2 mb-2"
+            >
+              Feedback
+            </a>
+            <span className="text-gray-400">|</span>
+            <a
+              href="/privacy-policy.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline hover:text-blue-500 mx-2 mb-2"
+            >
+              Support
+            </a>
+          </div>
         </div>
       </div>
     </div>
