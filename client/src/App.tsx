63f84cfe8a89c186538648b6f5239b7d8f913573
Chirag-Jani <chirag@empiricinfotech.com>
2025-01-03 10:12:58 +0530

    feat: Added the header content.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,10 @@ import Footer from "./layout/footer/Footer";
 import AllRoutes from "./routes/AllRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter as Router } from "react-router-dom"; 
+import { BrowserRouter as Router } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const App: React.FC = () => {
-
   useEffect(() => {
     // Disable right-click across the application
     const handleRightClick = (e: MouseEvent) => e.preventDefault();
@@ -17,18 +17,45 @@ const App: React.FC = () => {
     // Cleanup the event listener on component unmount
     return () => document.removeEventListener("contextmenu", handleRightClick);
   }, []);
-  
+
   return (
-    <Router>
-    <div className="flex flex-col min-h-screen bg-gray-800 overflow-x-hidden">
-      <div className="flex-grow">
-        <ConnectWallet />
-        <AllRoutes />
+    <>
+      <div>
+        <Helmet>
+          <title>Gully Buddy International</title>
+          <meta name="robots" content="index, follow" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="%PUBLIC_URL%/CDE.svg"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="512x512"
+            href="%PUBLIC_URL%/CDE.svg"
+          />
+          <meta
+            name="description"
+            content="Join our Web3-powered DApp membership platform for collaborative growth and exclusive perks. Earn residuals, explore retail, and unlock exclusive tools."
+          />
+        </Helmet>
       </div>
-      <Footer />
-      <ToastContainer />
-    </div>
-    </Router>
+
+      <Router>
+        <div className="flex flex-col min-h-screen bg-gray-800 overflow-x-hidden">
+          <div className="flex-grow">
+            <ConnectWallet />
+            <AllRoutes />
+          </div>
+          <Footer />
+          <ToastContainer />
+        </div>
+      </Router>
+    </>
   );
 };
 
