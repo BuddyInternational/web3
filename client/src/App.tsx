4d98dcd186a29184a653463573efaec445282436
Chirag-Jani <chirag@empiricinfotech.com>
2025-01-03 11:00:26 +0530

    feat: Added the header content.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,44 +19,39 @@ const App: React.FC = () => {
   }, []);
 
   return (
-      <div>
-        <Helmet>
-          <title>Gully Buddy International</title>
-          <meta name="robots" content="index, follow" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"
-          />
-          <meta
-            name="description"
-            content="Join our Web3-powered DApp membership platform for collaborative growth and exclusive perks. Earn residuals, explore retail, and unlock exclusive tools."
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="192x192"
-            href="%PUBLIC_URL%/CDE.svg"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="512x512"
-            href="%PUBLIC_URL%/CDE.svg"
-          />
-        </Helmet>
+    <Router>
+      <Helmet>
+        <title>Gully Buddy International</title>
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"
+        />
+        <meta
+          name="description"
+          content="Join our Web3-powered DApp membership platform for collaborative growth and exclusive perks. Earn residuals, explore retail, and unlock exclusive tools."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="192x192"
+          href="%PUBLIC_URL%/CDE.svg"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="512x512"
+          href="%PUBLIC_URL%/CDE.svg"
+        />
+      </Helmet>
 
-        <Router>
-        <div className="flex flex-col min-h-screen bg-gray-800 overflow-x-hidden">
-          <div className="flex-grow">
-            <ConnectWallet />
-            <AllRoutes />
-          </div>
-          <Footer />
-          <ToastContainer />
+      <div className="flex flex-col min-h-screen bg-gray-800 overflow-x-hidden">
+        <div className="flex-grow">
+          <ConnectWallet />
+          <AllRoutes />
         </div>
-      </Router>
+        <Footer />
+        <ToastContainer />
       </div>
-
-      
-
+    </Router>
   );
 };
 
